41b78734a971384ee07a7cae41a62b316844cf81
sumonbarai <sumonbarai78@gmail.com>
2023-04-07 12:19:02 +0600

    weather application completed

--- weather application/app.js
+++ weather application/app.js
@@ -9,35 +9,36 @@ const debounce = function (fn, delay) {
     }, delay);
   };
 };
-
+// input event handler
 document.getElementById("input-text").addEventListener(
   "input",
   debounce(() => {
     const inputText = document.getElementById("input-text").value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&units=metric&appid=28fd15358cdecbc1a1dfef367e71acef`;
-    console.log(inputText);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${
+      inputText || "khulna"
+    }&units=metric&appid=28fd15358cdecbc1a1dfef367e71acef`;
     // get fetch request
     fetch(url)
       .then((res) => res.json())
       .then((result) => validCityName(result))
-      .catch((err) => console.log(err.message));
+      .catch(() => errorHandler());
   }, 1000)
 );
 
 const validCityName = (result) => {
-  const { name: cityName, main, weather } = result;
+  const {
+    name: cityName,
+    main,
+    weather,
+    sys: { country },
+  } = result;
+  console.log(result);
   if (result.coord) {
-    document.getElementById("city-name").innerText = cityName;
+    document.getElementById("city-name").innerText = cityName + " ," + country;
     document.getElementById("date").innerText = todayDate();
     document.getElementById("temperature").innerText = main.temp;
     document.getElementById("celcius").innerHTML = "&#8451";
     document.getElementById("status").innerText = weather[0].main;
-  } else {
-    document.getElementById("city-name").innerText = "Invalid city Name";
-    document.getElementById("date").innerText = "";
-    document.getElementById("temperature").innerText = "";
-    document.getElementById("celcius").innerText = "";
-    document.getElementById("status").innerText = "";
   }
 };
 
@@ -72,4 +73,22 @@ const todayDate = () => {
   const month = months[now.getMonth()];
   const year = now.getFullYear();
   return `${day}, ${date} ${month},${year}`;
+};
+
+// error handle
+const errorHandler = () => {
+  document.getElementById("city-name").innerText = "Invalid city Name";
+  document.getElementById("date").innerText = "";
+  document.getElementById("temperature").innerText = "";
+  document.getElementById("celcius").innerText = "";
+  document.getElementById("status").innerText = "";
+};
+
+// only first time call in web page loaded
+const defaultCity = () => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=khulna&units=metric&appid=28fd15358cdecbc1a1dfef367e71acef`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((result) => validCityName(result))
+    .catch(() => errorHandler());
 };
