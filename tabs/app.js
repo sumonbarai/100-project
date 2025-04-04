3bcf8eba26dfcaa81c1b7edaec95c7d5d0eb9d53
sumonbarai <sumonbarai78@gmail.com>
2025-04-04 12:00:47 +0600

    tabs project updated

--- tabs/app.js
+++ tabs/app.js
@@ -1,20 +1,33 @@
+// function openTab(event, id) {
+//   const allBtn = event.target.parentElement.children;
+
+//   console.log(allBtn);
+//   const currentBtn = event.currentTarget;
+//   const allContent = document.querySelectorAll(".tab-content");
+//   const currentTabContent = document.getElementById(id);
+
+//   // for remove all active class
+//   for (let i = 0; i < allBtn.length; i++) {
+//     allBtn[i].classList.remove("active");
+//   }
+//   currentBtn.classList.add("active");
+
+//   // for display content
+//   for (let i = 0; i < allBtn.length; i++) {
+//     allContent[i].classList.remove("active-content");
+//   }
+//   currentTabContent.classList.add("active-content");
+// }
+
 function openTab(event, id) {
-  const allBtn = event.target.parentElement.children;
+  // active class
+  const targetBtn = event.target;
+  document.querySelector(".tab-btn .active").classList.remove("active");
+  targetBtn.classList.add("active");
 
-  console.log(allBtn);
-  const currentBtn = event.currentTarget;
-  const allContent = document.querySelectorAll(".tab-content");
-  const currentTabContent = document.getElementById(id);
-
-  // for remove all active class
-  for (let i = 0; i < allBtn.length; i++) {
-    allBtn[i].classList.remove("active");
-  }
-  currentBtn.classList.add("active");
-
-  // for display content
-  for (let i = 0; i < allBtn.length; i++) {
-    allContent[i].classList.remove("active-content");
-  }
-  currentTabContent.classList.add("active-content");
+  // active tab-content
+  document
+    .querySelector(".tab-content.active-content")
+    .classList.remove("active-content");
+  document.getElementById(id).classList.add("active-content");
 }
